b6a36f69278ffb425398f4962ea76350afedab70
msteiner <m.steiner.extern@senec.com>
2026-02-19 20:53:38 +0100

    add level2

--- scenes/Cutscenes/intro_2.gd
+++ scenes/Cutscenes/intro_2.gd
@@ -12,3 +12,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name=="intro2":
+		get_tree().change_scene_to_file("res://scenes/Level2.tscn")
---
+++ scenes/level_2.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+var player: Player
+@onready var center: Node2D = $Center
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var remote_transform_2d: RemoteTransform2D = $RemoteTransform2D
+@onready var you_died: YouDiedAnim = $Camera2D/YouDied
+@onready var camera_2d: Camera2D = $Camera2D
+
+func _ready() -> void:
+	animation_player.play("intro")
+
+func _on_sockcess_animation_done() -> void:
+	pass
+	#get_tree().change_scene_to_file("res://scenes/Cutscenes/intro2.tscn")
+	
+func spawn_player():
+	var scene = preload("res://scenes/player.tscn")
+	var player = scene.instantiate()
+	player.level="level2"
+	add_child(player)
+	player.position=remote_transform_2d.position
+	player.spawn=remote_transform_2d.position
+	player.died.connect(you_died.on_player_died)
+	you_died.you_died_played.connect(player.on_you_died_you_died_played)
+	remote_transform_2d.get_parent().remove_child(remote_transform_2d)
+	player.add_child(remote_transform_2d)
+	remote_transform_2d.remote_path=NodePath("../../Camera2D")
+	remote_transform_2d.position=Vector2.ZERO
+	remote_transform_2d.update_position=true
