87bd74d53273642bc4da14d10c8dcd642e5eddb0
maximilian <maximilian.steiner@iteratec.de>
2026-02-21 16:18:54 +0100

    decrease difficulty, return to start screen

--- scenes/Cutscenes/introlvl_3.gd
+++ scenes/Cutscenes/introlvl_3.gd
@@ -11,3 +11,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name=="beam":
+		get_tree().change_scene_to_file("res://scenes/start_screen.tscn")
